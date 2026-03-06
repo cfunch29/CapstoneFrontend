c852959752d142ef171feecd279ed8fadffe6491
cfunch29 <cfunch29@gmail.com>
2026-03-06 15:59:46 -0500

    Dashboard page

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,2 +1,17 @@
 import React, { useState } from "react";
 import LoginForm from "../../components/loginForm/LoginForm.jsx";
+import SignUp from "../../components/signUpForm/SignUp.jsx";
+
+const Auth = () => {
+    const [newUser, setNewUser] = useState(false);
+    return (
+        <>
+        {newUser ? (
+            <SignUp setNewUser={setNewUser}/> ) :
+            ( <LoginForm setNewUser={setNewUser} />
+            )}
+            </>
+    );
+};
+
+export default Auth;
