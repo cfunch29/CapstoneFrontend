8bf00b827d386fad3d24782d68251c37f3148702
cfunch29 <cfunch29@gmail.com>
2026-03-06 12:51:35 -0500

    added auth context and context provider code

--- src/context/authContext/AuthContext.jsx
+++ src/context/authContext/AuthContext.jsx
@@ -4,13 +4,35 @@ import axios from "axios";
 
 const AppContext = createContext();
 
-export const UserProvider = ({ children }) => {
+export const AuthProvider = ({ children }) => {
     const [cookies, setCookies, removeCookie] = useCookies();
 
+async function login(formData) {
+    let res = await axios.post("http://localhost:3000/api/auth", formData);
 
+    setCookies("token", res.data.token);
+}
 
+async function signUp(formData) {
+    let res = await axios.post("http://localhost:3000/api/user", formData);
 
+    setCookies("token", res.data.token);
+}
 
+function logout() {
+    ["token"].forEach((token) => removeCookie(token));
+}
+
+// use memo ---> stores a value from computationally functions and will not return those functions as long as the value doesn't change
+
+const value = useMemo(() => ({
+    cookies, 
+    login, 
+    logout,
+    signUp,
     
-    return <AppContext.Provider>{children}</AppContext.Provider>;
+}),
+[cookies],);
+
+    return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
